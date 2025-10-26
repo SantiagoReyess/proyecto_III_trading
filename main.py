8d94e20bcbf8b497ea3106b5a305e6e88199d41e
Santi Reyes <santiago.reyes@iteso.mx>
2025-10-25 23:14:21 -0600

    testing virtual environment

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from data_download import data_download
 from technical_indicators import get_signals
 from split import  split_ttv
 from data_labeling import label
+from model_training import prepare_data_for_model, create_dnn_model, create_cnn_model, create_lstm_model, get_model_creator, run_experiment
 
 
 def main():
@@ -16,9 +17,13 @@ def main():
     data = label(data, alpha=0.05)
 
     ## Prepare the data for the model
+    lookback_period = 5
+    X_train, y_train, X_test, y_test, X_val, y_val = prepare_data_for_model(data=data, lookback_period=lookback_period)
 
 
-    return print(data["signal"])
+
+
+    return print(X_train)
 
 if __name__ == "__main__":
     main()
